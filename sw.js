a7566e54f9023402df4e3429f231111d3c7153bf
umukoronelson39-design <umukoronelson39@gmail.com>
2026-01-24 16:22:00 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
-const cacheName = 'uplifted-nelson-cache-v1';
-const filesToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  // Add other files like CSS, JS here if needed
-];
+const cacheName = "uplifted-nelson-cache-v1";
+const filesToCache = ["/", "/index.html", "/manifest.json"];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll(filesToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
